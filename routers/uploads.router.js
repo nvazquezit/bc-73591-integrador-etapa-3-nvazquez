2b92ec7b7a851cefc72721a87fb3d722b278f38f
Nicolas Vazquez <vaznico93@hotmail.com>
2025-05-22 02:57:56 -0300

    hacemos funcionar el metodo post para la imagen

--- routers/uploads.router.js
+++ routers/uploads.router.js
@@ -1,24 +1,8 @@
 import express from 'express'
 const routerUpload = express.Router()
 import controller from '../controllers/uploads.controller.js'
-import path from 'node:path'
-import multer from 'multer'
-import { v4 as uuidv4 } from 'uuid';
+import uploadsMiddleware from '../midlewares/uploads.middleware.js'
 
-
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        const rutaDeAlmacenamiento = path.join('public', 'uploads')
-        cb(null, rutaDeAlmacenamiento)
-    },
-    filename: function(req, file, cb) {
-        const extension = file.originalname.split('.').pop()
-        const nombreArchivo = `${uuidv4()}.${extension}`
-        cb(null,nombreArchivo)
-}
-})
-
-const uploadsMiddleware = multer({ storage })
 
 routerUpload.post('/', uploadsMiddleware.single('foto'), controller.uploadImagen)
 
---
+++ utils/handle-storage.js
@@ -0,0 +1,18 @@
+import path from 'node:path'
+import multer from 'multer'
+import { v4 as uuidv4 } from 'uuid';
+
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        const rutaDeAlmacenamiento = path.join('public', 'uploads')
+        cb(null, rutaDeAlmacenamiento)
+    },
+    filename: function(req, file, cb) {
+        const extension = file.originalname.split('.').pop()
+        const nombreArchivo = `${uuidv4()}.${extension}`
+        cb(null,nombreArchivo)
+}
+})
+
+export default storage
