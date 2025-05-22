5c919e0caa74213a35772889675b0e2eddba7fc8
Nicolas Vazquez <vaznico93@hotmail.com>
2025-05-22 03:54:18 -0300

    generamos la uri remota

--- server.js
+++ server.js
@@ -5,17 +5,26 @@ import connection from './utils/connection.js'
 import routerProductos from './routers/productos.router.js'
 import routerUsuarios from './routers/usuarios.router.js'
 import routerUploads from './routers/uploads.router.js'
+import path from 'node:path'
+import cors from 'cors'
 
 // ! Constantes
 const app = express()
 const PORT = 8080
-const URI_DB = process.env.URI_LOCAL
+const URI_DB = process.env.URI_REMOTA
+const URL_FRONT = process.env.URL_FRONTEND_CORS
 //console.log(URI_DB)
 
 // ! Configuraciones
 
+const corsConfig = {
+  origin: URL_FRONT // prod: http://etapa-3.netlify.com/ | dev: http://localhost:5173
+}
+
 // ! Middlewares
 app.use(express.json()) // Traduce a Express el objeto que llega por el body
+app.use(express.static(path.join('public')))
+app.use(cors(corsConfig))
 
 // ! Rutas
 app.use('/api/v1/productos', routerProductos)
